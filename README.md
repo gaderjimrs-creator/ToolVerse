25f3dc85c343217688088b9fe275e924ffc6a73f
Mohammed Ayaz <gaderjimrs@gmail.com>
2026-06-16 22:31:46 +0530

    Phase 3 package modernization

--- README.md
+++ README.md
@@ -4,10 +4,16 @@ A personal Python utility library created by Mohammed Ayaz.
 
 ## Features
 
-- Calculator Tools
-- String Tools
-- File Tools
-- System Tools
+- conversion_tools.py
+- date_time_tools.py
+- file_tools.py
+- number_tools.py
+- password_tools.py
+- random_tools.py
+- string_tools.py
+- student_tools.py
+- system_tools.py
+- calculator.py
 
 ## Installation
 
--- src/MyPythonLibrary/__init__.py
+++ src/MyPythonLibrary/__init__.py
@@ -1,23 +1,25 @@
 from .calculator import MyCalculator
-from .string_tools import MyStringTools
-from .file_tools import MyFileTools
-from .system_tools import MySystemTools
-from .student_tools import MyStudentTools
-from .random_tools import MyRandomTools
-from .password_tools import MyPasswordTools
-from .number_tools import MyNumberTools
 from .conversion_tools import MyConversionTools
 from .date_time_tools import MyDateTimeTools
+from .file_tools import MyFileTools
+from .number_tools import MyNumberTools
+from .password_tools import MyPasswordTools
+from .random_tools import MyRandomTools
+from .string_tools import MyStringTools
+from .student_tools import MyStudentTools
+from .system_tools import MySystemTools
+
+__version__ = "1.1.0"
 
 __all__ = [
     "MyCalculator",
-    "MyStringTools",
-    "MyFileTools",
-    "MySystemTools",
-    "MyStudentTools",
-    "MyRandomTools",
-    "MyPasswordTools",
-    "MyNumberTools",
     "MyConversionTools",
     "MyDateTimeTools",
+    "MyFileTools",
+    "MyNumberTools",
+    "MyPasswordTools",
+    "MyRandomTools",
+    "MyStringTools",
+    "MyStudentTools",
+    "MySystemTools",
 ]
